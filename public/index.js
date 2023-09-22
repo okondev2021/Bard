5685b489d5024efe3c286f02315f55c0f4622c9f
okondev2021 <sojapril16@gmail.com>
2023-09-22 08:20:52 +0100

    bard

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 import Bard from "../node_modules/bard-ai/index.js";
 
-let myBard = new Bard("bAjOmqg_sqEJjQqNc3v38v5ddOkt8CB7lCGqXQhUQ4fEQoWGDt8IpS7xXrXitf96Nuk0Dg.")
+let myBard = new Bard("bQhs4exOT82Cw7aclmhD4vnZ1OLfufno3qDQrf-WNv5X4_VurlTbZouxnf6ow9nIQUp5RQ.")
 
 const response = await myBard.ask("What is the meaning of life?");
 
